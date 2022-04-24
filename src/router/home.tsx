1ab862c3018391c931cbb6b552209eda5b6fd9bb
gaoge <183412808@qq.com>
2022-04-25 03:12:52 +0800

    start

--- src/router/home.tsx
+++ src/router/home.tsx
@@ -1,7 +1,7 @@
 // const query_ = require("./sql.query.tsx");
 
 module.exports = router => {
-  router.get("/home/getUserSignature", async ctx => {
+  router.get("/home/getUserSignature", ctx => {
     // const data = await query_("SELECT * FROM user_info");
 
     // let { name, title, signature, avatar_url, qq_account, git_account, wechat_account, email_account, zfb_account } =
