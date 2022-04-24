926c5ad012a44eef3c8b4594fa69bb619b3d030f
gaoge <183412808@qq.com>
2022-04-25 03:31:34 +0800

    just try

--- src/router/home.tsx
+++ src/router/home.tsx
@@ -1,7 +1,19 @@
 // const query_ = require("./sql.query.tsx");
+const data = {
+  avatar: "https://img0.baidu.com/it/u=1801829287,2841444421&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
+  signature: "Don't cry, do laugh",
+  title: "说说我的生活",
+  userName: "高歌",
+  articlesNum: 10,
+  tabsNum: 20,
+  gitURL: "",
+  QQ: "183412808",
+  WeChat: "gaolaogui2",
+  Email: "183412808@qq.com"
+};
 
 module.exports = router => {
-  router.get("/home/getUserSignature", ctx => {
+  router.get("/home/getUserSignature", async ctx => {
     // const data = await query_("SELECT * FROM user_info");
 
     // let { name, title, signature, avatar_url, qq_account, git_account, wechat_account, email_account, zfb_account } =
@@ -19,17 +31,6 @@ module.exports = router => {
     //   WeChat: wechat_account,
     //   Email: email_account
     // };
-    ctx.body = {
-      avatar: "https://img0.baidu.com/it/u=1801829287,2841444421&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500",
-      signature: "Don't cry, do laugh",
-      title: "说说我的生活",
-      userName: "高歌",
-      articlesNum: 10,
-      tabsNum: 20,
-      gitURL: "",
-      QQ: "183412808",
-      WeChat: "gaolaogui2",
-      Email: "183412808@qq.com"
-    };
+    ctx.body = data;
   });
 };
