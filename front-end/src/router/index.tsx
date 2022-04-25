e1de3b608fda24cc5d441b36f903c42d04ef20fb
gaoge <183412808@qq.com>
2022-04-26 03:31:55 +0800

    update

--- front-end/src/router/index.tsx
+++ front-end/src/router/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import { HashRouter as Router } from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 
 export default function () {
