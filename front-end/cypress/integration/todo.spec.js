b77b9a5703ff606136ada655607eb06ba42c376b
gaolaoge <gaolaoge@sina.cn>
2022-04-30 05:54:53 +0800

    尝试加入cypress

---
+++ front-end/cypress/integration/todo.spec.js
@@ -0,0 +1,11 @@
+/// <refrence types="Cypress" />
+
+describe("初始化测试", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:2345");
+  });
+  it("测试DOM", () => {
+    cy.get("#root").find(".App");
+    cy.get(".App-link").should("contain", "Learn React");
+  });
+});
