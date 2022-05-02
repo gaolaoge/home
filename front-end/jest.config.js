e8ee4ee0a5ead50114ca5ddf8a3b1fe319db8f95
gaolaoge <gaolaoge@sina.cn>
2022-05-03 05:14:44 +0800

    试错

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -2,7 +2,8 @@ const config = {
   moduleNameMapper: {
     "\\.(css|less)$": "<rootDir>/empty.js"
   },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"]
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  modulePaths: ["src", "test"]
 };
 
 module.exports = config;
